22a0efb19793eacfdb3adab333de1a9c1b887f9f
Jo Onuma <joimportant@yahoo.co.uk>
2019-09-11 09:00:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,7 +4,7 @@ def fizzbuzz(int)
       "Fizz" # Go fizz
    elsif int % 5 == 0 # if the number int is divisible by 5
       "Buzz" # Go Buzz
-   elsif int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 15
+   elsif int % 3 == 0 || int % 5 == 0 # if the number int is divisible by 15
        "FizzBuzz" # Go FizzBuzz
    else int % 4 != 0
       nil
